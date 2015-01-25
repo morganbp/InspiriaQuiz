c255573dc3e94da056ab96a4296b69ed4c37db57
Phil <khrinx@gmail.com>
2015-01-25 17:07:46 +0100

    API Quiz.php v1.1 updated

--- DB/quiz.php
+++ DB/quiz.php
@@ -2,14 +2,14 @@
 header('Content-type: application/json');
 
 include("db_connect.php");
-if(!isset($_GET['action'])) {
+if(!isset($_POST['action'])) {
 	echo "No action set.\n\n";
-	echo "GET parameter 'action' (required): \n 'select' or 'insert'\n\n";
-	echo "GET parameter 'quizName' (required at inserts): \n Name of the quiz\n\n";
-	echo "GET parameter 'prettyPrint' (optional): \n anything\n\n";
+	echo "POST parameter 'action' (required): \n 'select' or 'insert'\n\n";
+	echo "POST parameter 'quizName' (required at inserts): \n Name of the quiz\n\n";
+	echo "POST parameter 'prettyPrint' (optional): \n anything\n\n";
 	die();
 }
-switch($_GET['action']){
+switch($_POST['action']){
 	case 'select':
 		if($stmt = $mysqli -> prepare("SELECT QuizID, QuizName FROM quiz")) {
 			$stmt -> execute();
@@ -22,21 +22,21 @@ switch($_GET['action']){
 			$stmt -> free_result();
 			$stmt -> close();
 
-			echo json_encode($output, (isset($_GET['prettyPrint'])?JSON_PRETTY_PRINT:0));
+			echo json_encode($output, (isset($_POST['prettyPrint'])?JSON_PRETTY_PRINT:0));
 		}else{
 			echo "Failed to prepare statement";
 		}
 	break;
 	
 	case 'insert':
-		$quizName = $_GET['quizName'];
+		$quizName = $_POST['quizName'];
 
 		if($stmt = $mysqli -> prepare("INSERT INTO quiz(QuizName) VALUES(?)")) {
 			$stmt -> bind_param("s", $quizName);
 
 			$stmt -> execute();
 			$stmt -> close();
-			echo "Quiz inserted";
+			echo "Quiz inserted.";
 		}else{
 			echo "Failed to prepare statement";
 		}
