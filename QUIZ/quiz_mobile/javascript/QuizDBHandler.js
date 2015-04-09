05e3477600d3902ad01007d2e4a9687eeb86449d
Karl Henrik <karl_henrik@live.no>
2015-04-09 12:24:21 +0200

    Old

--- QUIZ/quiz_mobile/javascript/QuizDBHandler.js
+++ QUIZ/quiz_mobile/javascript/QuizDBHandler.js
@@ -3,7 +3,7 @@ function QuizDBHandler(){
 	this.getQuizJsonByID = function(quizID, dataCollecter){
 		"use strict";
 		$.ajax({
-			url: /*"http://frigg.hiof.no/bo15-g21/API/quiz_get.php"*/ "http://localhost/inspiriaquiz/QUIZ/quiz_mobile/API/quiz_get.php", 
+			url: /*"http://frigg.hiof.no/bo15-g21/API/quiz_get.php"*/ "http://127.0.0.1:81/inspiriaquiz/QUIZ/quiz_mobile/API/quiz_get.php", 
 			data: {QuizID: quizID},
 			type: 'POST',
 			error: function(XMLHttpRequest, textStatus, errorThrown){
